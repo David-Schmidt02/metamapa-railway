c3d2df37485e943d50f94509360a8c89c27c7fc7
Thiago Perez Giacchetta <tperezgiacchetta@frba.utn.edu.ar>
2025-06-01 15:13:22 -0300

    Arreglo dependencia de EstaticaController, hay que seguir con la logica del Service y el importador

--- estatica/src/main/java/ar/edu/utn/frba/ddsi/estatica/controllers/EstaticaController.java
+++ estatica/src/main/java/ar/edu/utn/frba/ddsi/estatica/controllers/EstaticaController.java
@@ -3,6 +3,7 @@ package ar.edu.utn.frba.ddsi.estatica.controllers;
 import ar.edu.utn.frba.ddsi.estatica.services.IHechosServices;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.http.ResponseEntity;
 
 @RestController
--- estatica/src/main/java/ar/edu/utn/frba/ddsi/estatica/services/HechosServices.java
+++ estatica/src/main/java/ar/edu/utn/frba/ddsi/estatica/services/HechosServices.java
@@ -1,9 +1,11 @@
 package ar.edu.utn.frba.ddsi.estatica.services;
 import ar.edu.utn.frba.ddsi.estatica.models.repositories.HechosRepository;
 import ar.edu.utn.frba.ddsi.estatica.models.entities.hecho.Hecho;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
-
+@Service
 public class HechosServices implements IHechosServices{
     private HechosRepository hechosRepository;
 
