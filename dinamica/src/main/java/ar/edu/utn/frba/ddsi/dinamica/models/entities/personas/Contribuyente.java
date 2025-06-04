68aec92cfe361edbe73381fc2dfdb03b02c96dc9
BenicioGonzalez <bgonzalezcurcio@frba.utn.edu.ar>
2025-06-04 09:12:33 -0300

    agregue campos en contribuyente

--- dinamica/src/main/java/ar/edu/utn/frba/ddsi/dinamica/models/entities/personas/Contribuyente.java
+++ dinamica/src/main/java/ar/edu/utn/frba/ddsi/dinamica/models/entities/personas/Contribuyente.java
@@ -1,4 +1,13 @@
 package ar.edu.utn.frba.ddsi.dinamica.models.entities.personas;
 
 public class Contribuyente {
+    private String nombre;
+    private String email;
+    private int edad;
+
+    public Contribuyente(String nombre, String email, int edad){
+        this.nombre = nombre;
+        this.email = email;
+        this.edad = edad;
+    }
 }
