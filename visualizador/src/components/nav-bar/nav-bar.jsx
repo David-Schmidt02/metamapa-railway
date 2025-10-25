95d82289edcfafe632f539fe3d61ca95291d33e5
provoleta <buhlertomas@gmail.com>
2025-10-25 01:32:03 -0300

    Fix de nevbar y navegacion basica

--- visualizador/src/components/nav-bar/nav-bar.jsx
+++ visualizador/src/components/nav-bar/nav-bar.jsx
@@ -3,22 +3,33 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 import {Button, Form, NavDropdown} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
 
 function NavBar() {
+    const navigate = useNavigate()
+
     return (
         <Navbar expand="lg" data-bs-theme='prueba' >
             <Container fluid >
-                <Navbar.Brand href="#">MetaMapa</Navbar.Brand>
+                <Navbar.Brand href="/home">MetaMapa</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll" >
                     <Nav
                         className="ms-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
+                        style={{ maxHeight: '150px' }}
                         navbarScroll
+                        activeKey="/home"
+                        onSelect={(selectedKey) => navigate( `${selectedKey}`)}
                     >
-                        <Nav.Link href="#action1" variant='flat'>Home</Nav.Link>
-                        <Nav.Link href="#action2" >Estadisticas</Nav.Link>
-                        <Nav.Link href="#action3" >Iniciar Sesion</Nav.Link>
+                        <Nav.Item>
+                            <Nav.Link href="/hecho" >Buscar</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="/estadisticas" >Estadisticas</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item className='sesion'>
+                            <Nav.Link href="/home" >Iniciar Sesion</Nav.Link>
+                        </Nav.Item>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
